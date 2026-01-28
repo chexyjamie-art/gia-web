979e9bc69fe325119958ff36eca9c500c49e40ee
chexyjamie-art <chexyjamie@gmail.com>
2026-01-28 19:32:10 +0530

    Delete CategoryTabs.jsx

--- src/components/CategoryTabs.jsx
+++
@@ -1,9 +0,0 @@
-export default function CategoryTabs() {
-  return (
-    <div className="tabs">
-      {["All","Watches","Skincare","Electronics","Shoes"].map(t => (
-        <span key={t} className={t==="Watches" ? "active" : ""}>{t}</span>
-      ))}
-    </div>
-  );
-}
