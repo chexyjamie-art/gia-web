38b9d625aafcccd2501eb4814a979c2dbba8951f
chexyjamie-art <chexyjamie@gmail.com>
2026-01-28 16:01:25 +0530

    Create data/products.js

---
+++ src/data/products.js
@@ -0,0 +1,7 @@
+import img from "../assets/images/placeholder-watch.png";
+
+export default [
+  { id:1, name:"Fossil Grant Classic", price:"₹7,495 – 8,095", image: img },
+  { id:2, name:"Titan Edge Ultra Thin", price:"₹10,995 – 19,998", image: img },
+  { id:3, name:"Seiko Prospex Solar", price:"₹16,995 – 21,999", image: img }
+];
