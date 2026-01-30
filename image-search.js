c1f41928490ac6026a09fc27745487152c03c866
chexyjamie-art <chexyjamie@gmail.com>
2026-01-30 07:42:47 +0530

    Delete image-search.js

--- image-search.js
+++
@@ -1,9 +0,0 @@
-function imageSearch(file) {
-  if (!file) return;
-
-  alert("AI analyzing image...");
-
-  // Fake AI match logic
-  const randomProduct = products[Math.floor(Math.random() * products.length)];
-  renderProducts([randomProduct]);
-}
