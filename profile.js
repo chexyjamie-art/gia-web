77c1ebc002e33224bfba5f30afc5ae5d6381fc2a
chexyjamie-art <chexyjamie@gmail.com>
2026-01-31 16:00:00 +0530

    Update profile.js

--- profile.js
+++ profile.js
@@ -6,3 +6,28 @@ function logout() {
   alert("Logged out successfully");
   window.location.href = "index.html";
 }
+
+function saveProfile(){
+  const profileData = {
+    name: document.getElementById("name").value,
+    gender: document.getElementById("gender").value,
+    mobile: document.getElementById("mobile").value,
+    email: document.getElementById("email").value,
+    address: document.getElementById("address").value
+  };
+
+  localStorage.setItem("giaProfile", JSON.stringify(profileData));
+  alert("Profile Saved Successfully ✅");
+}
+
+window.onload = function(){
+  const saved = JSON.parse(localStorage.getItem("giaProfile"));
+
+  if(saved){
+    document.getElementById("name").value = saved.name;
+    document.getElementById("gender").value = saved.gender;
+    document.getElementById("mobile").value = saved.mobile;
+    document.getElementById("email").value = saved.email;
+    document.getElementById("address").value = saved.address;
+  }
+}
