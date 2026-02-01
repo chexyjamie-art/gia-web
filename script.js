86461a6136c937bc4c1fad01c7d5ded98916aa53
chexyjamie-art <chexyjamie@gmail.com>
2026-02-01 21:17:29 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+// Jab user 'Buy Now' button pe click karega
+document.querySelectorAll('.buy-btn').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('Product added to your wishlist!');
+    });
+});
+
+// Search functionality ka basic setup
+const searchBtn = document.querySelector('.search-container button');
+const searchInput = document.querySelector('.search-container input');
+
+searchBtn.addEventListener('click', () => {
+    if(searchInput.value !== "") {
+        alert('Searching for: ' + searchInput.value);
+    } else {
+        alert('Please enter something to search.');
+    }
+});
