f2983fffee0d18dc572a71756cf6f23e6fb40766
chexyjamie-art <chexyjamie@gmail.com>
2026-02-03 06:16:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
-function toggleBox(id) {
-    const box = document.getElementById(id);
-    box.style.display = box.style.display === "block" ? "none" : "block";
-}
-
-document.getElementById("dpUpload").addEventListener("change", function(e) {
-    const file = e.target.files[0];
-    if (file) {
-        document.getElementById("profileDp").src = URL.createObjectURL(file);
-    }
-});// Jab user 'Buy Now' button pe click karega
+// Jab user 'Buy Now' button pe click karega
 document.querySelectorAll('.buy-btn').forEach(button => {
     button.addEventListener('click', () => {
         alert('Product added to your wishlist!');
@@ -24,54 +14,5 @@ searchBtn.addEventListener('click', () => {
         alert('Searching for: ' + searchInput.value);
     } else {
         alert('Please enter something to search.');
-    }
-});
-
-// Sidebar Toggle
-const menuIcon = document.querySelector('.menu-icon');
-const sidebar = document.getElementById('sidebar');
-const closeMenu = document.getElementById('closeMenu');
-
-menuIcon.addEventListener('click', () => {
-    sidebar.style.left = "0";
-});
-
-closeMenu.addEventListener('click', () => {
-    sidebar.style.left = "-260px";
-});
-
-// Profile Popup Toggle
-const profileDp = document.querySelector('.profile-dp');
-const profilePopup = document.getElementById('profilePopup');
-
-profileDp.addEventListener('click', () => {
-    profilePopup.style.display =
-        profilePopup.style.display === "block" ? "none" : "block";
-});
-
-function openImageOptions() {
-    document.getElementById('dpUpload').click();
-}
-
-document.getElementById('dpUpload').addEventListener('change', function (e) {
-    const file = e.target.files[0];
-    if (!file) return;
-
-    const reader = new FileReader();
-    reader.onload = function () {
-        document.getElementById('profileImage').src = reader.result;
-    };
-    reader.readAsDataURL(file);
-});
-
-function toggleBox(id) {
-    const box = document.getElementById(id);
-    box.style.display = box.style.display === "block" ? "none" : "block";
-}
-
-document.getElementById("dpUpload").addEventListener("change", function(e) {
-    const file = e.target.files[0];
-    if (file) {
-        document.getElementById("profileDp").src = URL.createObjectURL(file);
     }
 });
