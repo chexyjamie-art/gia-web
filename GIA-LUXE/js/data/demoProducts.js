3e2a72b9bdcbfba7742a3ac345b7a51a4e78d7a6
chexyjamie-art <chexyjamie@gmail.com>
2026-03-01 06:11:24 +0530

    Create data/demoProducts.js

---
+++ GIA-LUXE/js/data/demoProducts.js
@@ -0,0 +1,37 @@
+export const demoProducts = [
+  {
+    id: 1,
+    name: "Classic White T-Shirt",
+    category: "topwear",
+    image: "assets/products/tshirt_white.png",
+    price: 799
+  },
+  {
+    id: 2,
+    name: "Blue Denim Jeans",
+    category: "bottomwear",
+    image: "assets/products/jeans_blue.png",
+    price: 1499
+  },
+  {
+    id: 3,
+    name: "Running Sneakers",
+    category: "footwear",
+    image: "assets/products/sneakers.png",
+    price: 2299
+  },
+  {
+    id: 4,
+    name: "Leather Watch",
+    category: "watch",
+    image: "assets/products/watch.png",
+    price: 1999
+  },
+  {
+    id: 5,
+    name: "Black Cap",
+    category: "hat",
+    image: "assets/products/cap.png",
+    price: 599
+  }
+];
